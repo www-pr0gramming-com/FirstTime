e9bc836250368786646e34f4d365b0f4e1492b5a
c0rat1e5 <cccc0rat1e5@icloud.com>
2022-02-24 13:49:11 +0900

    model admin

--- app/admin.py
+++ app/admin.py
@@ -1,9 +1,21 @@
 from django.contrib import admin
 from .models import User, A001, Agent, UserProfile, Category
 
+
+class A001Admin(admin.ModelAdmin):
+    fields = (
+        "first_name",
+        "last_name",
+    )
+
+    list_display = ["id", "first_name", "last_name", "age", "email"]
+    list_editable = ["first_name"]
+    list_filter = ["category"]
+    search_fields = ["first_name"]
+
+
 admin.site.register(User)
-admin.site.register(A001)
+admin.site.register(A001, A001Admin)
 admin.site.register(Agent)
 admin.site.register(UserProfile)
 admin.site.register(Category)
-
