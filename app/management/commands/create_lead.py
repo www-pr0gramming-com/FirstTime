1dc573830f68bff6d9cfcf6003f6f7880a4951f3
c0rat1e5 <cccc0rat1e5@icloud.com>
2022-02-26 12:51:18 +0900

    read csv file

--- app/management/commands/create_lead.py
+++ app/management/commands/create_lead.py
@@ -1,10 +1,32 @@
 from django.core.management.base import BaseCommand
+from csv import DictReader
+
+from app.models import UserProfile, A001
 
 
 class Command(BaseCommand):
     def add_arguments(self, parser):
-        parser.add_argument("first_name", type=str)
+        parser.add_argument("file_name", type=str)
+        parser.add_argument("organisor_email", type=str)
 
     def handle(self, *args, **options):
-        # print("hello world")
-        print(options["first_name"])
+        file_name = options["file_name"]
+        organisor_email = options["organisor_email"]
+        organisation = UserProfile.objects.get(user__email=organisor_email)
+
+        with open(file_name, "r") as read_obj:
+            csv_reader = DictReader(read_obj)
+            for row in csv_reader:
+                # print(row)
+                first_name = row["first_name"]
+                last_name = row["last_name"]
+                age = row["age"]
+                email = row["email"]
+
+                A001.objects.create(
+                    organisation=organisation,
+                    first_name=first_name,
+                    last_name=last_name,
+                    age=age,
+                    email=email,
+                )
